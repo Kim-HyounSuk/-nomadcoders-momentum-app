a22174afd75b07d5a83d65b86d9b0e40bc1c1d9e
Hyounsuk Kim <kkoor586@naver.com>
2023-05-03 00:49:41 +0900

    add notification

--- src/modules/Pomodoro.js
+++ src/modules/Pomodoro.js
@@ -5,6 +5,7 @@ const Pomodoro = ($container) => {
   const constructor = () => {
     template();
     render();
+    Notification.requestPermission();
   };
 
   const template = () => {
@@ -18,21 +19,32 @@ const Pomodoro = ($container) => {
       </div>
       <div class='pomo-setting'>
         <input type='number' value='30' min='0' max='60' required />
-        <button>Start</button>
+        <button class='pomo_btn--start'>Start</button>
+        <button class='pomo_btn--stop hidden'>Stop</button>
       </div>
     `;
     $container.appendChild($pomoBox);
   };
 
   const render = () => {
-    const pomoBtn = $container.querySelector(".pomo-setting button");
-    pomoBtn.addEventListener("click", () => {
+    const pomoBtnStart = $container.querySelector(".pomo_btn--start");
+    const pomoBtnStop = $container.querySelector(".pomo_btn--stop");
+    pomoBtnStart.addEventListener("click", () => {
       superEventHandler.onClickStart();
+    });
+    pomoBtnStop.addEventListener("click", () => {
+      superEventHandler.onClickStop();
     });
   };
 
   const startPomo = () => {
     if (timerId) return;
+    notiStart();
+    const pomoBtnStart = $container.querySelector(".pomo_btn--start");
+    const pomoBtnStop = $container.querySelector(".pomo_btn--stop");
+    pomoBtnStart.classList.add("hidden");
+    pomoBtnStop.classList.remove("hidden");
+
     const pomoInput = $container.querySelector(".pomo-setting input");
     const pomoTimerItems = $container.querySelectorAll(".pomo-timer span");
     pomoTime = parseInt(pomoInput.value) * 60;
@@ -45,39 +57,53 @@ const Pomodoro = ($container) => {
         pomoTimerItems[1].classList.add("active");
         pomoTimerItems[2].textContent = seconds.toString().padStart(2, "0");
       } else {
-        pomoTime = 30 * 60;
-        timerId = undefined;
-        const pomoBtn = $container.querySelector(".pomo-setting button");
-        pomoBtn.onClick = null;
-        pomoBtn.innerText = "Start";
-        pomoBtn.addEventListener("click", () => {
-          superEventHandler.onClickStart();
-        });
+        endPomo();
       }
     }, 1000);
+  };
+
+  const endPomo = () => {
+    notiEnd();
+    clearInterval(timerId);
+    pomoTime = 30 * 60;
+    timerId = undefined;
+    const pomoBtnStart = $container.querySelector(".pomo_btn--start");
+    const pomoBtnStop = $container.querySelector(".pomo_btn--stop");
+
+    pomoBtnStart.classList.remove("hidden");
+    pomoBtnStop.classList.add("hidden");
+  };
+
+  const notiStart = () => {
+    if (Notification.permission === "granted") {
+      new Notification("코어 타임 시작", {
+        body: "코어 타임 시작!",
+        icon: "https://www.google.com/favicon.ico",
+      });
+    } else {
+      alert("코어 타임이 시작 되었습니다.");
+    }
+  };
+
+  const notiEnd = () => {
+    if (Notification.permission === "granted") {
+      new Notification("코어 타임 종료", {
+        body: "코어 타임 종료!",
+        icon: "https://www.google.com/favicon.ico",
+      });
+    } else {
+      alert("코어 타임이 종료 되었습니다.");
+    }
   };
 
   const superEventHandler = {
     onClickStart: () => {
       startPomo();
-      const pomoBtn = $container.querySelector(".pomo-setting button");
-      pomoBtn.onClick = null;
-      pomoBtn.innerText = "Stop";
-      pomoBtn.addEventListener("click", () => {
-        superEventHandler.onClickStop();
-      });
     },
     onClickStop: () => {
+      endPomo();
       clearInterval(timerId);
-      pomoTime = 30 * 60;
-      timerId = undefined;
       const pomoTimerItems = $container.querySelectorAll(".pomo-timer span");
-      const pomoBtn = $container.querySelector(".pomo-setting button");
-      pomoBtn.onClick = null;
-      pomoBtn.innerText = "Start";
-      pomoBtn.addEventListener("click", () => {
-        superEventHandler.onClickStart();
-      });
       pomoTimerItems[0].innerText = "00";
       pomoTimerItems[1].classList.remove("active");
       pomoTimerItems[2].innerText = "00";
